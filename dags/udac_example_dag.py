f1d0eef6160dfb8ec3dc1fd4dd90977ef35ea060
veectro <veectro@gmail.com>
2022-03-16 00:54:49 +0100

    update: operator dependency

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -14,18 +14,26 @@ from helpers import SqlQueries
 default_args = {
     'owner': 'udacity',
     'start_date': datetime(2019, 1, 12),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_retry': False
 }
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
-        )
+          schedule_interval='0 * * * *',
+          catchup=False)
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
+    aws_credentials='aws_credentials',
+    redshift_conn='redshift',
+    s3_bucket='udacity-dend',
+    s3_key='log_data',
+    table='staging_events',
     dag=dag
 )
 
@@ -64,4 +72,11 @@ run_quality_checks = DataQualityOperator(
     dag=dag
 )
 
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
+
+start_operator \
+    >> [stage_events_to_redshift, stage_songs_to_redshift] \
+    >> load_songplays_table \
+    >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] \
+    >> run_quality_checks \
+    >> end_operator
--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,25 +1,25 @@
-from airflow.hooks.postgres_hook import PostgresHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.models import BaseOperator
-from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
 
-    @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 aws_credentials: str,
+                 redshift_conn: str,
+                 s3_bucket: str,
+                 s3_key: str,
+                 target_table: str,
                  *args, **kwargs):
-
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.aws_credentials = aws_credentials
+        self.redshift_conn = redshift_conn
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.target_table = target_table
 
     def execute(self, context):
         self.log.info('StageToRedshiftOperator not implemented yet')
-
 
 
 
