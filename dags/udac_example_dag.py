d7243403535fd0d05afd1c1aaf7e536ae21c3c81
veectro <veectro@gmail.com>
2022-03-17 22:58:26 +0100

    refactor: create_tables.sql in dags/sql/

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -27,96 +27,16 @@ dag = DAG('udac_example_dag',
 
 start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
 
+# Using PostgresOperator to create staging tables from sql file in dags/sql
+# Based on :
+# https://airflow.apache.org/docs/apache-airflow-providers-postgres/stable/operators/postgres_operator_howto_guide.html
 create_tables_in_redshift = PostgresOperator(
     task_id='Create_tables_in_postgres',
     dag=dag,
     postgres_conn_id='redshift_conn_id',
-    sql="""
-        CREATE TABLE IF NOT EXISTS public.artists (
-            artistid varchar(256) NOT NULL,
-            name varchar(256),
-            location varchar(256),
-            lattitude numeric(18,0),
-            longitude numeric(18,0)
-        );
-        
-        CREATE TABLE IF NOT EXISTS public.songplays (
-            playid varchar(32) NOT NULL,
-            start_time timestamp NOT NULL,
-            userid int4 NOT NULL,
-            "level" varchar(256),
-            songid varchar(256),
-            artistid varchar(256),
-            sessionid int4,
-            location varchar(256),
-            user_agent varchar(256),
-            CONSTRAINT songplays_pkey PRIMARY KEY (playid)
-        );
-        
-        CREATE TABLE IF NOT EXISTS public.songs (
-            songid varchar(256) NOT NULL,
-            title varchar(256),
-            artistid varchar(256),
-            "year" int4,
-            duration numeric(18,0),
-            CONSTRAINT songs_pkey PRIMARY KEY (songid)
-        );
-        
-        CREATE TABLE IF NOT EXISTS public.staging_events (
-            artist varchar(256),
-            auth varchar(256),
-            firstname varchar(256),
-            gender varchar(256),
-            iteminsession int4,
-            lastname varchar(256),
-            length numeric(18,0),
-            "level" varchar(256),
-            location varchar(256),
-            "method" varchar(256),
-            page varchar(256),
-            registration numeric(18,0),
-            sessionid int4,
-            song varchar(256),
-            status int4,
-            ts int8,
-            useragent varchar(256),
-            userid int4
-        );
-        
-        CREATE TABLE IF NOT EXISTS public.staging_songs (
-            num_songs int4,
-            artist_id varchar(256),
-            artist_name varchar(256),
-            artist_latitude numeric(18,0),
-            artist_longitude numeric(18,0),
-            artist_location varchar(256),
-            song_id varchar(256),
-            title varchar(256),
-            duration numeric(18,0),
-            "year" int4
-        );
-        
-        CREATE TABLE IF NOT EXISTS public."time" (
-            start_time timestamp NOT NULL,
-            "hour" int4,
-            "day" int4,
-            week int4,
-            "month" varchar(256),
-            "year" int4,
-            weekday varchar(256),
-            CONSTRAINT time_pkey PRIMARY KEY (start_time)
-        );
-        
-        CREATE TABLE IF NOT EXISTS public.users (
-            userid int4 NOT NULL,
-            first_name varchar(256),
-            last_name varchar(256),
-            gender varchar(256),
-            "level" varchar(256),
-            CONSTRAINT users_pkey PRIMARY KEY (userid)
-        );
-        """
+    sql='sql/create_tables.sql'
 )
+
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
     aws_credentials='aws_conn_id',
