4956beba499d58ccdb4cb2b4db46aad35dffea27
veectro <veectro@gmail.com>
2022-03-17 22:48:25 +0100

    feat: add staging_redshift

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -1,11 +1,14 @@
 from datetime import datetime, timedelta
 import os
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.dummy import DummyOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from operators.stage_redshift import StageToRedshiftOperator
 from operators.load_fact import LoadFactOperator
 from operators.load_dimension import LoadDimensionOperator
 from operators.data_quality import DataQualityOperator
+from operators.create_table import CreateTableOperator
 from helpers import SqlQueries
 
 default_args = {
@@ -24,6 +27,96 @@ dag = DAG('udac_example_dag',
 
 start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
 
+create_tables_in_redshift = PostgresOperator(
+    task_id='Create_tables_in_postgres',
+    dag=dag,
+    postgres_conn_id='redshift_conn_id',
+    sql="""
+        CREATE TABLE IF NOT EXISTS public.artists (
+            artistid varchar(256) NOT NULL,
+            name varchar(256),
+            location varchar(256),
+            lattitude numeric(18,0),
+            longitude numeric(18,0)
+        );
+        
+        CREATE TABLE IF NOT EXISTS public.songplays (
+            playid varchar(32) NOT NULL,
+            start_time timestamp NOT NULL,
+            userid int4 NOT NULL,
+            "level" varchar(256),
+            songid varchar(256),
+            artistid varchar(256),
+            sessionid int4,
+            location varchar(256),
+            user_agent varchar(256),
+            CONSTRAINT songplays_pkey PRIMARY KEY (playid)
+        );
+        
+        CREATE TABLE IF NOT EXISTS public.songs (
+            songid varchar(256) NOT NULL,
+            title varchar(256),
+            artistid varchar(256),
+            "year" int4,
+            duration numeric(18,0),
+            CONSTRAINT songs_pkey PRIMARY KEY (songid)
+        );
+        
+        CREATE TABLE IF NOT EXISTS public.staging_events (
+            artist varchar(256),
+            auth varchar(256),
+            firstname varchar(256),
+            gender varchar(256),
+            iteminsession int4,
+            lastname varchar(256),
+            length numeric(18,0),
+            "level" varchar(256),
+            location varchar(256),
+            "method" varchar(256),
+            page varchar(256),
+            registration numeric(18,0),
+            sessionid int4,
+            song varchar(256),
+            status int4,
+            ts int8,
+            useragent varchar(256),
+            userid int4
+        );
+        
+        CREATE TABLE IF NOT EXISTS public.staging_songs (
+            num_songs int4,
+            artist_id varchar(256),
+            artist_name varchar(256),
+            artist_latitude numeric(18,0),
+            artist_longitude numeric(18,0),
+            artist_location varchar(256),
+            song_id varchar(256),
+            title varchar(256),
+            duration numeric(18,0),
+            "year" int4
+        );
+        
+        CREATE TABLE IF NOT EXISTS public."time" (
+            start_time timestamp NOT NULL,
+            "hour" int4,
+            "day" int4,
+            week int4,
+            "month" varchar(256),
+            "year" int4,
+            weekday varchar(256),
+            CONSTRAINT time_pkey PRIMARY KEY (start_time)
+        );
+        
+        CREATE TABLE IF NOT EXISTS public.users (
+            userid int4 NOT NULL,
+            first_name varchar(256),
+            last_name varchar(256),
+            gender varchar(256),
+            "level" varchar(256),
+            CONSTRAINT users_pkey PRIMARY KEY (userid)
+        );
+        """
+)
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
     aws_credentials='aws_conn_id',
@@ -31,6 +124,7 @@ stage_events_to_redshift = StageToRedshiftOperator(
     s3_bucket='udacity-dend',
     s3_key='log_data',
     target_table='staging_events',
+    redshift_arn=Variable.get('redshift_iam_arn'),
     dag=dag
 )
 
@@ -40,7 +134,8 @@ stage_songs_to_redshift = StageToRedshiftOperator(
     redshift_conn='redshift_conn_id',
     s3_bucket='udacity-dend',
     s3_key='song_data',
-    target_table='stage_songs',
+    target_table='staging_songs',
+    redshift_arn=Variable.get('redshift_iam_arn'),
     dag=dag
 )
 
@@ -77,8 +172,9 @@ run_quality_checks = DataQualityOperator(
 end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
 
 start_operator \
-    >> [stage_events_to_redshift, stage_songs_to_redshift] \
-    >> load_songplays_table \
-    >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] \
-    >> run_quality_checks \
-    >> end_operator
+>> create_tables_in_redshift \
+>> [stage_events_to_redshift, stage_songs_to_redshift] \
+>> load_songplays_table \
+>> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] \
+>> run_quality_checks \
+>> end_operator
--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -15,6 +15,9 @@ class StageToRedshiftOperator(BaseOperator):
                  s3_bucket: str,
                  s3_key: str,
                  target_table: str,
+                 redshift_arn:str,
+                 region: str = 'us-west-2',
+                 json_format = 'auto',
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.aws_credentials = aws_credentials
@@ -22,16 +25,27 @@ class StageToRedshiftOperator(BaseOperator):
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
         self.target_table = target_table
+        self.redshift_arn = redshift_arn
+        self.region = region
+        self.json_format = json_format
 
     def execute(self, context):
         self.log.info('StageToRedshiftOperator not implemented yet')
 
-        # Getting credentials from AWS Connection
-        s3_hook = S3Hook(self.aws_credentials)
-        pprint(s3_hook.get_credentials())
-        s3_keys = s3_hook.list_keys(self.s3_bucket, self.s3_key)
-        pprint(s3_keys)
-        redshift = PostgresHook(connection=self.redshift_conn)
+        # Connecting to Redshift
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn)
         pprint(self.redshift_conn)
         pprint(os.environ)
+
+        # Creating staging table
+        # based on https://docs.aws.amazon.com/redshift/latest/dg/copy-parameters-data-source-s3.html
+        # for authentication : https://docs.aws.amazon.com/redshift/latest/dg/copy-parameters-authorization.html
+        copy_query = f"""
+            COPY {self.target_table}
+            FROM 's3://{self.s3_bucket}/{self.s3_key}'
+            IAM_ROLE '{self.redshift_arn}'
+            REGION AS '{self.region}'
+            FORMAT as json '{self.json_format}'
+        """
+        redshift_hook.run(copy_query)
 
